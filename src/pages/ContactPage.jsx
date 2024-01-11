6649ace39e656ef02d2d6d4fd5e1407cf8d79eb8
milan98080 <mee.lawn222@gmail.com>
2024-01-12 01:34:00 +0545

    send mails from contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,58 +1,58 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useRef } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
 import TopBar from '../components/TopBar'
 import { SlLocationPin } from "react-icons/sl";
 import { CiMobile1, CiMail } from "react-icons/ci";
 import Links from '../components/Links';
+import emailjs from '@emailjs/browser';
 
 const ContactPage = () => {
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    title: '',
-    message: ''
-  });
+  const form = useRef();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    console.log(formData)
 
-    try {
-      // Replace 'YOUR_SERVER_ENDPOINT' with the actual endpoint where your server handles email sending
-      await axios.post('YOUR_SERVER_ENDPOINT', formData);
-
-      // Optionally, you can handle success here (e.g., show a success message, redirect the user, etc.)
-      console.log('Email sent successfully!');
-    } catch (error) {
-      // Handle errors here (e.g., show an error message to the user)
-      console.error('Error sending email:', error);
-    }
+    emailjs.sendForm('service_6vis7xu', 'template_qr0skco', form.current, '7D5qxkpi2qqsJbFPR')
+      .then((result) => {
+        toast.success('Message sent successfully !');
+        form.current.reset();
+      }, (error) => {
+        toast.error('Something went wrong !');
+      });
   };
 
   return (
     <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: "",
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+        }}
+      />
       <TopBar />
       <div className='w-full flex items-center justify-center'>
         <div className='w-[85%] h-[70dvh] my-12 bg-neutral-800/50 rounded-2xl border border-neutral-400 py-2 md:py-10 lg:py-20 flex flex-wrap overflow-scroll'>
           <div className='w-full pl-4 md:pl-24 mb-0 md:mb-4 lg:mb-8 '><Links /></div>
-          <form onSubmit={handleSubmit} className='form w-full md:w-[60%] pl-4 pr-4 md:pr-0 md:pl-24'>
+          <form ref={form} onSubmit={sendEmail} className='form w-full md:w-[60%] pl-4 pr-4 md:pr-0 md:pl-24'>
             <h1 className=' text-white font-semibold text-3xl md:text-4xl lg:text-5xl'>Contact Me !</h1>
             <h1 className=' text-neutral-400 text-xl md:text-2xl mt-1 md:mt-4'>Would love to hear from you.</h1>
             <div className=' w-full flex'>
-              <input type='text' name='name' id='name' placeholder='Name' value={formData.name}
-                onChange={handleChange} className='w-[40%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-8 mr-2 p-2' />
-              <input type='email' name='email' id='email' value={formData.email}
-                onChange={handleChange} placeholder='Email' className='w-[60%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-8 p-2' />
+              <input required minLength={3} type='text' name='user_name' id='name' placeholder='Name' className='w-[40%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-8 mr-2 p-2' />
+              <input required type='email' name='user_email' id='email' placeholder='Email' className='w-[60%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-8 p-2' />
             </div>
-            <input type='text' name='title' id='title' placeholder='Title' value={formData.title}
-              onChange={handleChange} className='w-[100%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-4 p-2' />
-            <textarea type='text' name='message' id='message' placeholder='Message' value={formData.message}
-              onChange={handleChange} className='w-[100%] max-h-[200px] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-4 p-2' />
+            <input required minLength={3} type='text' name='topic' id='title' placeholder='Title' className='w-[100%] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-4 p-2' />
+            <textarea required minLength={5} type='text' name='message' id='message' placeholder='Message' className='w-[100%] max-h-[200px] bg-neutral-500 border rounded-xl border-neutral-400 text-white text-xl focus:outline-none focus:border-white mt-4 p-2' />
             <div className='w-full flex items-center justify-center mt-6'>
               <button type='submit' className=' bg-black border border-neutral-400 rounded-3xl text-neutral-400 px-10 py-2'>Send Message</button>
             </div>
